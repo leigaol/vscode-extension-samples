9eef1c57f0c6443bf8dceb328d6e5b0ed94ce8d7
Lei Gao <leigaol@amazon.com>
2023-11-06 16:35:23 -0800

    add custom api
    add custom api
    
    add select

--- helloworld-sample/src/extension.ts
+++ helloworld-sample/src/extension.ts
@@ -2,6 +2,18 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+export interface Customization {
+    arn: string;
+    name?: string;
+    description?: string;
+}
+export type Customizations = Customization[];
+
+interface AwsToolkitApi {
+	setCustomization(customization: Customization): boolean
+	listAvailableCustomizations(): Customizations
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -9,15 +21,33 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "helloworld-sample" is now active!');
 
+
+
+
+	const ext = vscode.extensions.getExtension<AwsToolkitApi>('amazonwebservices.aws-toolkit-vscode');
+	const api = ext?.exports;
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('extension.list', async () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		const r = await api?.listAvailableCustomizations();
+		console.log(r);
 	});
 
-	context.subscriptions.push(disposable);
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable2 = vscode.commands.registerCommand('extension.select', async () => {
+		// The code you place here will be executed every time your command is executed
+		const r = await api?.listAvailableCustomizations();
+		if (r && r[2]){
+			console.log(`Selecting ${r[2].arn}`);
+			await api?.setCustomization(r[2]);
+		}
+	});
+	context.subscriptions.push(disposable2);
 }
